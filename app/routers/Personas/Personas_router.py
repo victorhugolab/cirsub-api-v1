97bf7f48abb740001a59b764bef426d0deda48c4
viktor <victorhugo.lab@gmail.com>
2025-08-07 13:38:50 -0300

    Modificacion mensajes de rutas para swagger

--- app/routers/Personas/Personas_router.py
+++ app/routers/Personas/Personas_router.py
@@ -12,16 +12,16 @@ from controllers.Personas.Personas_controller import (
 router = APIRouter(prefix="/personas", tags=["Personas"])
 
 
-@router.post("/ou",summary="Obtener un persona ingresando su ID", response_model=None)
+@router.post("/ou",summary="Obtiene una persona ingresando su Id.", response_model=None)
 async def personas_ou(request: Request, body: PersonasOUInModel = Body(...)):
     return await personas_controller_ou(request, body)
 
 
-@router.post("/in", summary="Insertar un registro en Personas", response_model=None)
+@router.post("/in", summary="Inserta un registro en Personas.", response_model=None)
 async def personas_ou(request: Request, body: PersonasINModel = Body(...)):
     return await personas_controller_in(request, body)
 
-@router.post("/cbu", summary="Inserta un CBU en Personas_cbu", response_model=None)
+@router.post("/cbu", summary="Inserta un CBU en Personas_CBU.", response_model=None)
 async def personas_ou(request: Request, body: PersonasCbuModel = Body(...)):
     return await personas_controller_CBU(request, body)
 
